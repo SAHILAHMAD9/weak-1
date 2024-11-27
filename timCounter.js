62ba1ace2f1777609e03842607060fa39f457960
SAHIL AHMAD <sahilahmad3504@gmail.com>
2024-11-28 04:25:54 +0530

    Update timCounter.js

--- timCounter.js
+++ timCounter.js
@@ -3,11 +3,13 @@ function printfn() {
   console.clear();
   console.log(counter);
   counter += 1;
+  // 2=> setTimeout(printfn, 1000);
 }
 for (let i = 1; i <= 100; i++) {
   setTimeout(printfn, i*1000);
 }
 
+// 2=> setTimeout(printfn, 1000);
 
 // OR use setInterval();
 
